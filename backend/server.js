d4f2be4597ddfad34df723bdaed245955e029d5d
musakazim989 <musakazim989@gmail.com>
2022-09-14 02:40:00 +0600

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 const express = require("express")
-const { readdirSync } = require("fs")
-const cors = require("cors")
+const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-
+const cors = require("cors")
+const { readdirSync } = require("fs")
 const app = express()
+
 dotenv.config()
+
+// middleware
 app.use(cors())
 
+// routes
 readdirSync("./routes").map((f) => app.use("/", require("./routes/" + f)))
+
+// database
+mongoose.connect(process.env.MONGODB_CONNECTON_URL).then(() => {
+  console.log("MongoDB connected")
+})
 
 const port = process.env.PORT || 8000
 
