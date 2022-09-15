a985f99bca1aad8a4b25e508e32dd4a9457fa9f0
musakazim989 <musakazim989@gmail.com>
2022-09-16 03:34:01 +0600

    Schema set and error found

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -1,3 +1,29 @@
- exports.register = (req, res) => {
-  res.send("Hello World from controllers")
+const User = require("../models/User.js")
+
+exports.register = async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    username,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  } = req.body
+
+  const user = await new User({
+    first_name,
+    last_name,
+    email,
+    password,
+    username,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  }).save()
+
+  res.json(user)
 }
--- backend/models/User.js
+++ backend/models/User.js
@@ -85,7 +85,7 @@ const userSchema = mongoose.Schema(
       {
         user: {
           type: mongoose.Schema.Types.ObjectId,
-          ref: User,
+          ref: "User",
         },
       },
     ],
